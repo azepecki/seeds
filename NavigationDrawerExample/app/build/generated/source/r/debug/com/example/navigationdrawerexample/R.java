6cf783166590e69800678999989074575774acd3
SONIA UPPAL <sonia9uppal@gmail.com>
2015-07-01 21:54:32 -0700

    Framework of the Play View

--- NavigationDrawerExample/app/build/generated/source/r/debug/com/example/navigationdrawerexample/R.java
+++ NavigationDrawerExample/app/build/generated/source/r/debug/com/example/navigationdrawerexample/R.java
@@ -32,12 +32,25 @@ public final class R {
         public static final int ic_launcher=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0006;
+        public static final int ScrollView01=0x7f0a0004;
+        public static final int action_settings=0x7f0a0013;
         public static final int content_frame=0x7f0a0001;
         public static final int drawer_layout=0x7f0a0000;
-        public static final int imageViewIcon=0x7f0a0004;
+        public static final int imageButton=0x7f0a0006;
+        public static final int imageButton2=0x7f0a0007;
+        public static final int imageButton3=0x7f0a000c;
+        public static final int imageButton4=0x7f0a0009;
+        public static final int imageButton5=0x7f0a000a;
+        public static final int imageButton6=0x7f0a000d;
+        public static final int imageButton7=0x7f0a000f;
+        public static final int imageButton8=0x7f0a0010;
+        public static final int imageViewIcon=0x7f0a0011;
         public static final int left_drawer=0x7f0a0002;
-        public static final int textViewName=0x7f0a0005;
+        public static final int linearLayout=0x7f0a0005;
+        public static final int linearLayout2=0x7f0a0008;
+        public static final int linearLayout3=0x7f0a000b;
+        public static final int linearLayout4=0x7f0a000e;
+        public static final int textViewName=0x7f0a0012;
         public static final int txtLabel=0x7f0a0003;
     }
     public static final class layout {
